91bb59226bba58a56fe22555ad7be035dd1cbfde
Fabio Sassu <fabio.sassu@hotmail.it>
2021-12-06 20:06:26 +0100

    View Dimension In Collapsed State. (#9)
    - fixed an issue that could cause the view to display greater cards in collapsed mode.
    - project cleaning.
    - updated library version to 1.0.2.
    - turned sample textview into material button.
    - updated dependencies.

--- app/src/main/java/it/fabiosassu/demo/MainActivity.kt
+++ app/src/main/java/it/fabiosassu/demo/MainActivity.kt
@@ -1,63 +1,57 @@
 package it.fabiosassu.demo
 
 import android.os.Bundle
-import android.util.TypedValue
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintLayout.LayoutParams
 import androidx.core.view.ViewCompat
 import it.fabiosassu.demo.databinding.ActivityMainBinding
+import it.fabiosassu.demo.databinding.ActivityMainBinding.inflate
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: it.fabiosassu.demo.databinding.ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     var counter = STARTING_ITEM_NUMBER
-    private val padding: Int
-        get() = TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            8.toFloat(),
-            resources.displayMetrics
-        ).toInt()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        // set data
-        binding.horizontalStack.setWidgets(getLayouts(false))
-        binding.verticalStack.setWidgets(getLayouts())
-        // add items on button click
-        binding.addGroupBtn.setOnClickListener {
-            counter++
-            binding.verticalStack.addWidget(getLayout(counter))
-            binding.horizontalStack.addWidget(getLayout(counter, false))
+        with(binding) {
+            // set data
+            horizontalStack.setWidgets(getLayouts(false))
+            verticalStack.setWidgets(getLayouts())
+            // add items on button click
+            addGroupBtn.setOnClickListener {
+                counter++
+                verticalStack.addWidget(getLayout(counter))
+                horizontalStack.addWidget(getLayout(counter, false))
+            }
         }
     }
 
     private fun getLayouts(isVertical: Boolean = true) = mutableListOf<ItemView>().apply {
         for (index in 1..STARTING_ITEM_NUMBER) {
-            this.add(getLayout(index, isVertical))
+            add(getLayout(index, isVertical))
         }
     }.toList()
 
-    private fun getLayout(index: Int, isVertical: Boolean = true): ItemView {
-        return ItemView(this@MainActivity).apply {
+    private fun getLayout(index: Int, isVertical: Boolean = true) =
+        ItemView(this@MainActivity).apply {
             id = ViewCompat.generateViewId()
-            layoutParams = if (isVertical) ConstraintLayout.LayoutParams(
-                ConstraintLayout.LayoutParams.MATCH_PARENT,
-                ConstraintLayout.LayoutParams.WRAP_CONTENT,
-            ).apply {
-                setPadding(0, padding, 0, padding)
-            } else ConstraintLayout.LayoutParams(
-                ConstraintLayout.LayoutParams.WRAP_CONTENT,
-                ConstraintLayout.LayoutParams.MATCH_PARENT,
-            ).apply {
-                setPadding(padding, 0, padding, 0)
+            if (isVertical) {
+                MATCH_PARENT to WRAP_CONTENT
+            } else {
+                WRAP_CONTENT to MATCH_PARENT
+            }.let { (width, height) ->
+                layoutParams = LayoutParams(width, height)
             }
             text = "Layout $index"
         }
-    }
 
     companion object {
         const val STARTING_ITEM_NUMBER = 30
